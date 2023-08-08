e344bba7d46ab8670c9ff500e5882a93aa8bcc47
flashcy <140628455+flashcy@users.noreply.github.com>
2023-08-08 20:47:42 +0900

    Feat/#1 (#2)
    * modify test
    
    * delete 불필요

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -7,7 +7,7 @@ function Cover() {
     return (
         <div className={styles.container}>
             <h3 className={styles.title} ref={target}>PREPARE SOCIAL HACKATHON</h3>
-            <span>24-25 AUGUST 2023</span>
+            <span>24-25 8ugust 2023</span>
             <button className={styles.btn}>VIEW MORE</button>
         </div>
     );
