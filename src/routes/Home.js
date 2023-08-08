721f1bb95fb037b41e70d032dd671ea3845bba58
flashcy <cksdud7890@naver.com>
2023-08-08 20:44:11 +0900

    delete 불필요

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from '../components/Card';
 import Header from '../components/Header';
-import styles from './Home.module.css';
+import styles from '../styles/Home.module.css';
 import Cover from '../components/Cover';
 
 function Home() {
